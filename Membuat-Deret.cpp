b735c94134daa93c0c7422399d5d1d1da9b1434a
XDevice-code <51826174+XDevice-code@users.noreply.github.com>
2020-10-27 20:47:23 +0700

    Create Membuat-Deret.cpp

---
+++ Membuat-Deret.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    int barisderet, n, x, y;
+
+    cout << "Baris Deret Yang Muncul Yaitu :";
+    cin>>barisderet; y=7;
+    for(x=1; x<deret; x++)
+    {
+    cout <<y<<"";y=y+7;
+
+    };
+    return 0;
+}
